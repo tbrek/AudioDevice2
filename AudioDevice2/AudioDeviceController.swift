f424dad5afd27c72dafdf4b4f4e8f0d3d4af7391
Tomasz <tomasz.brek@moogosoft.com>
2019-09-06 16:53:26 +0100

    Fix for constant

--- AudioDevice2/AudioDeviceController.swift
+++ AudioDevice2/AudioDeviceController.swift
@@ -574,7 +574,7 @@ class AudioDeviceController: NSObject {
             &defaultOutputDeviceID)
         
         var volume = Float32(0.50) // 0.0 ... 1.0
-        var volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
+        let volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
         
         var volumePropertyAddress = AudioObjectPropertyAddress(
             mSelector: kAudioHardwareServiceDeviceProperty_VirtualMasterVolume,
